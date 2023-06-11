a23f80ca6931720886e57fca1f70e167170c0ccc
hanggoro-litbang <125229690+hanggoro-litbang@users.noreply.github.com>
2023-06-11 19:59:21 +0700

    Update download_hima.sh

--- download_hima.sh
+++ download_hima.sh
@@ -14,13 +14,13 @@ cd ${hima_dir}/${yyyy1}${mm1}${dd1}${cc}
                echo "File NC_H09_${yyyy1}${mm1}${dd1}_${cc}00_R21_FLDK.02401_02401.nc exist, skipping"
        else
                echo "Download Temperature Brightness"
-               wget --user=izzaroyan_yahoo.com --password='SP+wari8' ftp://ftp.ptree.jaxa.jp/jma/netcdf/${yyyy1}${mm1}/${dd1}/NC_H09_${yyyy1}${mm1}${dd1}_${cc}00_R21_FLDK.02401_02401.nc
+               wget --user=user --password='password' ftp://ftp.ptree.jaxa.jp/jma/netcdf/${yyyy1}${mm1}/${dd1}/NC_H09_${yyyy1}${mm1}${dd1}_${cc}00_R21_FLDK.02401_02401.nc
        fi
 
        if [ -e ${hima_dir}/${yyyy1}${mm1}${dd1}${cc}/NC_H09_${yyyy1}${mm1}${dd1}_${cc}00_L2CLP010_FLDK.02401_02401.nc ]; then
                echo "File NC_H09_${yyyy1}${mm1}${dd1}_${cc}00_L2CLP010_FLDK.02401_02401.nc exist, skipping"
        else
                echo "Download Cloud Product"
-                wget --user=izzaroyan_yahoo.com --password='SP+wari8' ftp://ftp.ptree.jaxa.jp/pub/himawari/L2/CLP/010/${yyyy1}${mm1}/${dd1}/${cc}/NC_H09_${yyyy1}${mm1}${dd1}_${cc}00_L2CLP010_FLDK.02401_02401.nc
+                wget --user=user --password='password' ftp://ftp.ptree.jaxa.jp/pub/himawari/L2/CLP/010/${yyyy1}${mm1}/${dd1}/${cc}/NC_H09_${yyyy1}${mm1}${dd1}_${cc}00_L2CLP010_FLDK.02401_02401.nc
        fi
 exit 0
